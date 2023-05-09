0f4a4cd86efd3af84d4177bd347fc41165e033d6
Bautista Tosi <tosibautista@gmail.com>
2023-05-09 17:35:26 -0300

    ahora la forma de limitar los productos en el get general es a traves de el metodo filter si el index es menor que el limit retorne los productos

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -6,26 +6,26 @@ import { newMessage } from "../utils.js"
 productsRouter.get("/", (req, res) => {
     const products = list.getProducts()
     const { limit } = req.query
-    const productsLimited = products.filter((pro) => Number(pro.id) < limit)
-    const messageProductsLimited = newMessage("success","listado de productos limitados", productsLimited)
-    const messageAllProducts = newMessage("success","listado de productos", list.getProducts())
+    const productsLimited = products.filter((pro, index) => index < limit)
+    const messageProductsLimited = newMessage("success", "listado de productos limitados", productsLimited)
+    const messageAllProducts = newMessage("success", "listado de productos", list.getProducts())
     return res.status(200).json(Number(limit) ? messageProductsLimited : messageAllProducts)
 })
 productsRouter.get("/:pid", (req, res) => {
     const Id = req.params.pid
     return res.status(200).json(newMessage("success","producto por id", list.getProductById(Id)))
 })
-productsRouter.post("/", async(req, res) =>  {
+productsRouter.post("/", async (req, res) => {
     const newProduct = req.body
     const { title, description, price, thumbnail, code, stock } = newProduct
     return res.status(200).json(await list.addProduct(title, description, price, thumbnail, code, stock))
 })
-productsRouter.put("/:pid", async(req, res) => {
+productsRouter.put("/:pid", async (req, res) => {
     const Id = req.params.pid
     const productPropsToUpdate = req.body
-    return res.status(200).json(newMessage(await list.updateProduct(Id,productPropsToUpdate)))
+    return res.status(200).json(newMessage(await list.updateProduct(Id, productPropsToUpdate)))
 })
-productsRouter.delete("/:pid", async(req, res) => {
+productsRouter.delete("/:pid", async (req, res) => {
     const Id = req.params.pid
     return res.status(200).json(await list.deleteProduct(Id))
 })
