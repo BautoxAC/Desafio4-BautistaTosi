f7a841c98d9bbbb91546257757db28428f6e1fff
Bautista Tosi <tosibautista@gmail.com>
2023-05-27 21:53:21 -0300

    ahora el socketServer se ejecuta mediante una fuincion que se encuentra en el archivo productsSocketRouter para una mayor organizacion

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import { Server } from "socket.io"
 import handlebars from "express-handlebars"
 import { productRouterHtml } from "./routes/productRouterHtml.router.js"
 import { productsSocketRouter } from "./routes/productsSocketRouter.router.js"
-import { ProductManager } from "./ProductManager.js"
+import { functionSocketServer } from "./routes/productsSocketRouter.router.js"
 const app = express()
 const port = 8080
 
@@ -33,20 +33,11 @@ app.use("/realtimeproducts", productsSocketRouter)
 const httpServer = app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
 })
+
 const socketServer = new Server(httpServer)
-socketServer.on("connection", (socket) => {
-    console.log("cliente conectado")
-    const list = new ProductManager("src/public/products.json")
-    socket.on("msg_front_to_back", async (data) => {
-        const { title, description, price, thumbnails, code, stock } = data.data
-        socket.emit("newProduct_to_front",await list.addProduct(title, description, price, thumbnails, code, stock ),list.getProducts())
-    })
-    socket.emit("msg_back_to_front_products",list.getProducts())
-    socket.on("msg_front_to_back_delete_product", async(product)=>{
-        await list.deleteProduct(product.id)
-        socket.emit("msg_front_to_back_deleted",list.getProducts())
-    })
-})
+
+// Execute SocketSever in Rute /realtimeserver
+functionSocketServer(socketServer)
 
 app.get("*", (req, res) => {
     return res.status(404).json({
--- src/routes/productsSocketRouter.router.js
+++ src/routes/productsSocketRouter.router.js
@@ -1,9 +1,28 @@
 import express from "express"
 export const productsSocketRouter = express.Router()
 import { ProductManager } from "../ProductManager.js"
-
 productsSocketRouter.get('/', function (req, res) {
     const list = new ProductManager("src/public/products.json")
     const products = list.getProducts()
     return res.status(200).render("realTimeProducts", { products })
 })
+export function functionSocketServer(socketServer) {
+    socketServer.on("connection", (socket) => {
+        console.log("cliente conectado")
+        const list = new ProductManager("src/public/products.json")
+        socket.on("msg_front_to_back", async (data) => {
+            try {
+                const { title, description, price, thumbnails, code, stock } = data.data
+                socket.emit("newProduct_to_front", await list.addProduct(title, description, price, thumbnails, code, stock), list.getProducts())
+            } catch (e) {
+                console.log(e)
+                socket.emit("newProduct_to_front", { status: "failure", message: "something went wrong :(", data: {} })
+            }
+        })
+        socket.emit("msg_back_to_front_products", list.getProducts())
+        socket.on("msg_front_to_back_delete_product", async (product) => {
+            await list.deleteProduct(product.id)
+            socket.emit("msg_front_to_back_deleted", list.getProducts())
+        })
+    })
+}
